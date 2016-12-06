37a5258e55070be114bdf24090aa16c359181156
Jonathan Heek <jonathanheek@Jonathans-MacBook-Pro.local>
2016-12-06 10:37:57 +0100

    add fsync

--- test.sh
+++ test.sh
@@ -4,12 +4,12 @@ mkdir ext4
 sudo mount -t ucrfs -o size=64m ucrfs ./ramdisk
 
 echo "timing of ucrfs"
-sudo dd if=/dev/zero of=./ramdisk/test bs=1M count=256
+sudo dd conv=fsync if=/dev/zero of=./ramdisk/test bs=1M count=256
 
 sudo umount ./ramdisk
 
 echo "timing of ext4"
-sudo dd if=/dev/zero of=./ext4/test bs=1M count=256
+sudo dd conv=fsync if=/dev/zero of=./ext4/test bs=1M count=256
 
 rm -f ./ext4/test
 
